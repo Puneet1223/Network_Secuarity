2792980554d23e886bbe78998a07c28b164e52b9
Puneet Chhonker <puneetchhonker@Puneets-Air.lan>
2026-06-02 11:27:54 +0530

    trainer and eveluate ml flow experiments completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecuarity.components.data_ingestion import DataIngestion 
 from networksecuarity.components.data_validation import DataValidation
 from networksecuarity.components.data_transformation import DataTransformation
-from networksecuarity.entity.config_entity import DataIngestionConfig,DataTransformationConfig,DataValidationConfig
+from networksecuarity.components.model_trainer import ModelTrainer
+from networksecuarity.entity.config_entity import DataIngestionConfig,DataTransformationConfig,DataValidationConfig,ModelTrainerConfig
 from networksecuarity.entity.config_entity import TrainingPipelineConfig
 import sys
 from networksecuarity.exception.exception import NetworkSecuarityException
@@ -30,6 +31,14 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation Completed")
+        
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+      
 
 
     except Exception as e:
--- networksecuarity/components/data_ingestion.py
+++ networksecuarity/components/data_ingestion.py
@@ -1,24 +1,22 @@
 from networksecuarity.exception.exception import NetworkSecuarityException
 from networksecuarity.logging.logger import logging
 
-## configration  of Data Ingestion config
 
+## configuration of the Data Ingestion Config
 
 from networksecuarity.entity.config_entity import DataIngestionConfig
 from networksecuarity.entity.artifact_entity import DataIngestionArtifact
-
 import os
 import sys
 import numpy as np
 import pandas as pd
-import pymongo 
+import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
-
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 
 
 class DataIngestion:
@@ -29,33 +27,37 @@ class DataIngestion:
             raise NetworkSecuarityException(e,sys)
         
     def export_collection_as_dataframe(self):
+        """
+        Read data from mongodb
+        """
         try:
             database_name=self.data_ingestion_config.database_name
             collection_name=self.data_ingestion_config.collection_name
+            print("DB CL ",database_name,collection_name)
             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
             collection=self.mongo_client[database_name][collection_name]
 
             df=pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"])
-            return df    
-
-            df.replace({"na":np.nan},inplace=True)    
+            
+            df.replace({"na":np.nan},inplace=True)
+            return df
         except Exception as e:
-            raise NetworkSecuarityException(e,sys)
+            raise NetworkSecuarityException
         
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
             #creating folder
-            dir_path=os.path.dirname(feature_store_file_path)
-            os.makedirs(dir_path,exist_ok=True) 
-            dataframe.to_csv(feature_store_file_path,index=False,header=True)   
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
+            
         except Exception as e:
             raise NetworkSecuarityException(e,sys)
         
-
     def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
@@ -80,10 +82,13 @@ class DataIngestion:
             test_set.to_csv(
                 self.data_ingestion_config.testing_file_path, index=False, header=True
             )
-            logging.info(f"Exported train and test file path.")  
+            logging.info(f"Exported train and test file path.")
+
+            
         except Exception as e:
             raise NetworkSecuarityException(e,sys)
-            
+        
+        
     def initiate_data_ingestion(self):
         try:
             dataframe=self.export_collection_as_dataframe()
@@ -92,6 +97,6 @@ class DataIngestion:
             dataingestionartifact=DataIngestionArtifact(train_file_path=self.data_ingestion_config.training_file_path,
                                                         test_file_path=self.data_ingestion_config.testing_file_path)
             return dataingestionartifact
+
         except Exception as e:
-            raise NetworkSecuarityException(e,sys) 
-            
+            raise NetworkSecuarityException(e,sys)
